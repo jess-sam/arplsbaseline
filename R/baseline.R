e5741126ca52e9f52045cd07b7509af4139e3c8e
jess-sam <jess.sam823@gmail.com>
2025-08-26 23:42:45 +1200

    basic summary function for Spectrum and edited arguments to split up the raw data

--- R/baseline.R
+++ R/baseline.R
@@ -7,20 +7,34 @@ library("Rcpp")
 #' @return num
 #' @export
 
-Spectrum <- function(baseline, raw_data) {
+Spectrum <- function(baseline, x, y, corrected) {
   structure(
     list(
       baseline = baseline,
-      raw_data = raw_data
+      x = x,
+      y = y,
+      corrected = corrected
     ),
     class = "Spectrum"
   )
 }
 
-baseline <- function(data, lambda, ratio) {
-  
-  baseline_data <- rcpp_baseline(data, lambda, ratio)
-  spec <- Spectrum(baseline_data, data)
-  return(spec)
+#' @export
+summary.Spectrum <- function(spectrum) {
+  x <- spectrum$corrected
+  cat("---------Summary of Corrected Spectrum----------\n")
+  cat("Mean: ", round(mean(x),2), "\n")
+  cat("Standard Deviation:", round(sd(x),2),"\n")
+  cat("Maximum Signal", max(x), "\n")
+  cat("Minimum Signal", min(x), "\n")
+  cat("Range of Signals", range(x), "\n")
 }
 
+baseline <- function(data, lambda, ratio) {
+  x <- data[[1]]
+  y <- data[[2]]
+  baseline_data <- rcpp_baseline(y, lambda, ratio)
+  corrected <- y - baseline_data
+  spec <- Spectrum(baseline_data, x, y, corrected)
+  return(spec)
+}
