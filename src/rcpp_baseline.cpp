5166f0201bc07ba13c4e9a91cd4a7d2f10baf45a
jess-sam <jess.sam823@gmail.com>
2025-09-26 20:55:56 +1200

    removed copies for speed, used a sparse matrix instead of dense, added max iterations of 150

--- src/rcpp_baseline.cpp
+++ src/rcpp_baseline.cpp
@@ -12,9 +12,9 @@ arma::vec rcpp_baseline(const NumericVector &y_vec,
   auto start = chrono::high_resolution_clock::now();
   
   int N = y_vec.size();
-  arma::vec y = as<arma::vec>(y_vec);
+  arma::vec y(as<arma::vec>(y_vec));
   
-  NumericMatrix D(N - 2, N);
+  arma::sp_mat D(N-2, N);
   
   for(int i = 0; i < N - 2; i++){
     D(i, i) = 1.0;
@@ -22,24 +22,23 @@ arma::vec rcpp_baseline(const NumericVector &y_vec,
     D(i, i + 2) = 1.0;
   }
   
-  arma::mat D_arma(D.begin(), D.nrow(), D.ncol());
-  arma::mat H = lambda * D_arma.t() * D_arma;
-  arma::vec w = arma::ones(N);
-  arma::vec z = w;
+  arma::sp_mat H(lambda * D.t() * D);
+  arma::vec w(arma::ones(N));
+  arma::vec z(w);
   
   int iters = 1;
   
-  while(true){
-
+  while(iters < 150){
+    
     arma::mat W(N, N, arma::fill::zeros);
     W.diag() = w;
     
-    arma::mat C = arma::chol(W + H);
+    arma::mat C(arma::chol(W + H));
     z = arma::solve(C, arma::solve(C.t(), (w % y)));
 
-    arma::vec d = y-z;
-    arma::uvec ids = arma::find(d < 0); // Find indices
-    arma::vec d_minus = d.elem(ids);       // Assign value to condition
+    arma::vec d(y-z);
+    arma::uvec ids(arma::find(d < 0)); // Find indices
+    arma::vec d_minus(d.elem(ids));       // Assign value to condition
     
     double m = 0.0;
     double std = 0.0;
@@ -49,18 +48,26 @@ arma::vec rcpp_baseline(const NumericVector &y_vec,
       std = arma::stddev(d_minus);
     }
     
-    arma::mat wt = 1/ (1 + arma::exp(2 * (d-(2*std-m))/std));
+    arma::mat wt(1 /(1 + arma::exp(2 * (d-(2*std-m))/std)));
     
     if(norm(w - wt)/norm(w) < ratio){
       break;
     }
+    
     w = wt;
     iters++;
   }
+  
   auto end = chrono::high_resolution_clock::now();
   std::chrono::duration<double> time = end - start;
   
   Rcout << "Baseline Computation Time (seconds) = " << time.count() << "\n";
   Rprintf("Iterations = %d\n", iters);
+  
+  if(iters == 150){
+    Rprintf("Max iterations of the algorithm reached.\n");
+    
+  }
+  
   return z;
 }
