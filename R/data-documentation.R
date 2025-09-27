4911101bc884f98e9e54aa08e0a689e140bcec90
jess-sam <jess.sam823@gmail.com>
2025-09-27 14:26:33 +1200

    dataset upload and docs

---
+++ R/data-documentation.R
@@ -0,0 +1,72 @@
+#' Mid-Infrared Spectrum of Ham
+#'
+#' Fingerprint region of Mid-Infrared Spectrum of cooked British Ham, 
+#' using FTIR spectroscopy with ATR sampling (diamond).
+#' 
+#' @docType data
+#'
+#' @usage ham
+#'
+#' @format A dataframe with 2489 observations on 2 variables:
+#' \describe{
+#'   \item{Wavenumbers}{numeric Wavenumbers (1/cm)}
+#'   \item{Ham}{numeric Signal Intensity}
+#'   }
+#' @keywords datasets
+#'
+#' @source \href{https://github.com/QIBChemometrics/FTIR-food-library/blob/main/FTIR_Library_Spectra.csv}{QIB Chemometrics}
+#'
+
+"ham"
+
+#' Mid-Infrared Spectrum of Red Wine
+#'
+#' Fingerprint region of Mid-Infrared Spectrum of Australian Cabernet Sauvignon Red Wine, 
+#' using FTIR spectroscopy with ATR sampling (zinc selenide).
+#' 
+#' @docType data
+#'
+#' @usage redwine
+#'
+#' @format A dataframe with 235 observations on 2 variables.
+#' \describe{
+#'   \item{Wavenumbers}{numeric Wavenumbers (1/cm)}
+#'   \item{Wine}{numeric Signal Intensity}
+#'   }
+#'
+#' @keywords datasets
+#'
+#' @references Kemsley EK, Defernez M, Marini, F (2019). 
+#' Multivariate statistics: Considerations and confidences in food authenticity problems. 
+#' Food Control, 105, 102-112.
+#'
+#' @source \href{https://github.com/QIBChemometrics/FTIR-food-library/blob/main/FTIR_Library_Spectra.csv}{QIB Chemomentrics}
+#'
+
+"redwine"
+
+#' Mid-Infrared Spectrum of Strawberry Puree
+#'
+#' Mid-Infrared Spectrum of Strawberry Puree, 
+#' using FTIR spectroscopy with ATR sampling.
+#' 
+#' @docType data
+#'
+#' @usage strawberry
+#'
+#' @format A dataframe with 235 observations on 2 variables.
+#' \describe{
+#'   \item{Wavenumbers}{numeric Wavenumbers (1/cm)}
+#'   \item{Strawberry}{numeric Signal Intensity}
+#'   }
+#'  
+#' @keywords datasets
+#'
+#' @references Holland JK, Kemsley EK, Wilson RH. (1998). Use of Fourier transform 
+#'  infrared spectroscopy and partial least squares regression for the detection of 
+#'  adulteration of strawberry purees. Journal of the Science of Food and Agriculture, 76, 263-269.
+#'
+#' @source \href{https://csr.quadram.ac.uk/example-datasets-for-download/}{QIB Chemometrics}
+#' 
+
+"strawberry"
