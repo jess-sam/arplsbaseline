40d7b04743250cd8ae627f0331e3eeb803148d34
jess-sam <jess.sam823@gmail.com>
2025-10-15 01:55:03 +1300

    added masking functionality to baseline

--- R/baseline.R
+++ R/baseline.R
@@ -5,6 +5,8 @@
 #'
 #' @param data Dataframe of wavenumbers with corresponding signal intensity, first column must contain wavenumbers and second column signal intensity.
 #' @param lambda Regularisation term to control the smoothness of the baseline, default is 1e4, common values to use are in powers of 10, from 1e2 to 1e8. Larger values give smoother baselines.
+#' @param start_mask Lower limit of wavenumbers to ignore in baseline algorithm
+#' @param end_mask Upper limit of wavenumbers to ignore in baseline algorithm
 #' @return Spectrum Object of custom Spectrum class
 #' @examples
 #' # example code
@@ -14,8 +16,10 @@
 #' baseline(strawberry)
 #' @export 
 
-baseline <- function(data, lambda = 1e4) {
-
+baseline <- function(data, lambda = 1e4, start_mask = 0, end_mask = 0) {
+  
+  mask = FALSE
+  
   if (missing(data)) {
     stop("Data argument cannot be empty")
   }
@@ -76,14 +80,55 @@ baseline <- function(data, lambda = 1e4) {
     message("Computation in process! Please wait patiently! :D")
   }
   
+  alg_y <- y
+  
+  if (!missing(start_mask) & missing(end_mask) | missing(start_mask) & !missing(end_mask)) {
+    message("One of the start or end limits for masking is missing, no masking will be used.")
+  }
+  
+  if (!missing(start_mask) & !missing(end_mask)) {
+    
+    if (!is.numeric(start_mask) | !is.numeric(end_mask)) {
+      message("At least one of the masking limits is not numeric, no masking will be used.")
+      mask = FALSE
+    }
+    else if(start_mask > end_mask) {
+      message("Starting wavenumber to mask should not be larger than the end wavenumber, no masking will be used.")
+      mask = FALSE
+    }
+    else if (start_mask < min(x) | start_mask > max(x) | end_mask < min(x) | end_mask > max(x)) {
+      message("At least one of the masking limits is out of the range of the presented wavenumbers, no masking will be used.")
+      mask = FALSE
+    }
+    else {
+      mask = TRUE
+    }
+  }
+
+  if(mask){
+    masked_indices <- which(x >= start_mask & x <= end_mask)
+    alg_y <- y[-masked_indices]
+  }
+
+  if(length(alg_y) < 10) {
+    stop("There must be at least 10 non-masked entries")
+  }
+  
   baseline_data <- tryCatch(
   {
-    rcpp_baseline(y, lambda, 1e-4)
+    rcpp_baseline(alg_y, lambda, 1e-4)
   },
   error = function(e) {
     stop("Rcpp failed :( try again with another dataset: ", conditionMessage(e))
   }
 )
+  if (mask) {
+    baseline_complete <- rep(0, length(y))      
+    baseline_complete[masked_indices] <- 0        
+    baseline_complete[-masked_indices] <- baseline_data 
+    baseline_data <- baseline_complete
+  }
+  
   corrected <- y - baseline_data
   # vectors for consistency
   spec <- Spectrum(as.vector(baseline_data), x, y, as.vector(corrected))
